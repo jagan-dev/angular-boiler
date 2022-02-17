25b131543553c53c29f615c70b8243185fc6ac7f
jagan-dev <jaganuidev@gmail.com>
2022-02-17 18:58:54 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [				
+    AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      MenuBarComponent,
+      NavBarComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
