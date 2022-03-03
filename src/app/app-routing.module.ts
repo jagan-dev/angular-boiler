ab7fc43e39b2bef5b64a161f360d3691d3722906
jagan-dev <72982060+jagan-dev@users.noreply.github.com>
2022-03-03 21:33:32 +0530

    Revert "Merge pull request #9 from jagan-dev/revert-8-Hari"
    This reverts commit ac44b7d06cc7320ecfd25b5fad25affd261ef85e, reversing
    changes made to de3bfd97dab8e65a702f3e0eb8e2d2c5f78c5617.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuyingSellingComponent } from './buying-selling/buying-selling.component';
+import { GuestComponent } from './guest/guest.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'nav-bar',component:NavBarComponent},
+  {path:'guest',component:GuestComponent},
+  {path:'buy',component:BuyingSellingComponent}
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/buying-selling/buying-selling.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buying-selling',
+  templateUrl: './buying-selling.component.html',
+  styleUrls: ['./buying-selling.component.css']
+})
+export class BuyingSellingComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
