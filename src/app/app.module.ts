af6097baf1ba26388d8a79546bf873970bf09c05
Senthur-pandi <96013578+Senthur-pandi@users.noreply.github.com>
2022-03-04 13:29:51 +0530

    header,footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,13 +16,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DialogComponent } from './dialog/dialog.component';
 import {MatDialogModule} from '@angular/material/dialog';
 import { GuestComponent } from './guest/guest.component';
-import { BuyingSellingComponent } from './buying-selling/buying-selling.component';
+import { BuyingSellingComponent } from './guest/buying-selling/buying-selling.component';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { NavComponent } from './guest/nav/nav.component';
 import { ContentComponent } from './guest/content/content.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'nav-bar',component:NavBarComponent},
+  {path:'guest',component:GuestComponent},
+  {path:'buy',component:BuyingSellingComponent}
+ ];
 
 @NgModule({
   declarations:[
@@ -35,9 +42,7 @@ import { RouterModule } from '@angular/router';
       GuestComponent,
       BuyingSellingComponent,
       NavComponent,
-      ContentComponent,
-    
-      
+      ContentComponent, 
    ],
   imports: [
     BrowserModule,
@@ -52,7 +57,8 @@ import { RouterModule } from '@angular/router';
     MatSelectModule,
     MatSidenavModule,
     MatExpansionModule,
-    MatGridListModule
+    MatGridListModule,
+    RouterModule.forRoot(routes)
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
